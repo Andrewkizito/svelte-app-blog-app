03ec25e67cd24a73c0cb7c6bbe8bae6e9909fce4
andrewkizito <andrewkizito54@gmail.com>
2024-04-17 12:58:23 +0300

    feat: added input

--- src/components/ui/input.svelte
+++ src/components/ui/input.svelte
@@ -1,35 +1,23 @@
-<script>
-    /**
-     * @typedef {{
-     *   label: string,
-     *   type: string,
-     *   required: boolean,
-     *   placeholder: string
-     * }} Props
-     * @param {Props} props
-     */
-    export const props = {
-        label: 'Label',
-        type: 'text',
-        required: false,
-        placeholder: ''
-    };
+<script lang="ts">
+	import type { HTMLInputAttributes } from 'svelte/elements';
+
+	export let label: string;
+	export let inputProps: HTMLInputAttributes;
+    export let onValueChange: (value: string) => void
+
+    function handleInputChange(e: any){
+        inputProps.value = e
+        onValueChange(e)
+    }
 </script>
 
-<div class="flex flex-col gap-2">
-    <label for="input" class="block text-sm font-medium leading-6 text-gray-900">{props.label}</label>
-    <div class="mt-2">
-        <input
-            id="input"
-            name="input"
-            type={props.type}
-            required={props.required}
-            placeholder={props.placeholder}
-            class="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-        />
-    </div>
+<div class="flex flex-col gap-1">
+	<label for="input" class="block text-sm font-medium leading-6 text-gray-900">{label}</label>
+	<div class="mt-2">
+		<input
+			class="block px-2 w-full rounded-md border-0 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+			{...inputProps}
+            on:input={(e) => handleInputChange(e.currentTarget.value)}
+		/>
+	</div>
 </div>
-
-<style>
-    /* You can add any custom styles here */
-</style>
