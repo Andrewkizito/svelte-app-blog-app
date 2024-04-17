289511910277fbd5cf27a072ab55b6ea3e036b65
andrewkizito <andrewkizito54@gmail.com>
2024-04-17 15:01:13 +0300

    feat: cleanup

--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.6QfsR4tM.js","_app/immutable/chunks/scheduler.CV3NVII-.js","_app/immutable/chunks/index.BT3kTLlT.js","_app/immutable/chunks/entry.B7TqZKXO.js"];
-export const stylesheets = [];
-export const fonts = [];
